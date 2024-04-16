80c2dc79fdb93c21221ccfc3ee4fc9dad70e32cc
Andrew Pham <andrew.pham@shopify.com>
2024-04-15 22:10:18 -0400

    Scrape the price of bitcoin and ethereum

--- scrape.go
+++ scrape.go
@@ -12,12 +12,22 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	// Define Bitcoin and Ethereum selectors
+	bitcoinSelector := "table > tbody > tr:nth-child(1) > td:nth-child(4) > div > a > span"
+	ethereumSelector := "table > tbody > tr:nth-child(2) > td:nth-child(4) > div > a > span"
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// On visiting the main page, look for the table and the necessary rows using the defined selectors
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		bitcoinPrice := e.ChildText(bitcoinSelector)
+		ethereumPrice := e.ChildText(ethereumSelector)
+
+		fmt.Printf("Bitcoin Price: %s\n", bitcoinPrice)
+		fmt.Printf("Ethereum Price: %s\n", ethereumPrice)
+	})
+
+	// Log errors if there are any
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +36,5 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://coinmarketcap.com/")
 }
